5dc47043d07c708db33610a910fe304bca37c7c2
Arcee Juan <arceetaraguajuan@gmail.com>
2024-08-27 23:24:06 +0800

    model added

--- moa/agent/moa.py
+++ moa/agent/moa.py
@@ -29,14 +29,14 @@ class MOAgentConfig(BaseModel):
 
 load_dotenv()
 valid_model_names = Literal[
+    'llama-3.1-405b',
     'llama3-70b-8192',
     'llama3-8b-8192',
     'gemma-7b-it',
     'gemma2-9b-it',
     'mixtral-8x7b-32768',
     'llama-3.1-8b-instant',
-    'llama-3.1-70b-versatile',
-    'llama-3.1-405b'
+    'llama-3.1-70b-versatile'
 ]
 
 class ResponseChunk(TypedDict):
